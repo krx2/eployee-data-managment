db9d01626830fa07a840101f904747dc9e57daef
Konrad Zebro <konrad.zebro@gmail.com>
2026-07-13 13:38:32 +0200

    initial commit

---
+++ src/main/java/com/krx2/emplyeedatamanagment/EmplyeeDataManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.krx2.emplyeedatamanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplyeeDataManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmplyeeDataManagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krx2/emplyeedatamanagment/EmplyeeDataManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krx2.emplyeedatamanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmplyeeDataManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
